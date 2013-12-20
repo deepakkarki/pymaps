a56ca14097a1d0fb51fc1aff088d7a82fc4986e7
deepakkarki <deepak6k@gmail.com>
2013-12-20 23:04:15 +0530

    Added basic functionality to Graph class

--- src/Graph.py
+++ src/Graph.py
@@ -1,5 +1,6 @@
 #Graph class for holding the road network
 from xml.etree import ElementTree
+import copy
 
 class Graph(object):
 
@@ -30,42 +31,100 @@ class Graph(object):
 		'''
 		pass
 		
-	def add_node(self, Node):
+	def add_node(self, node):
 		'''
 		add a node to the given graph
 		'''
-		pass
+		if node.pid not in self.n_pid:
+			self.n_pid[node.pid] = node
+			pos = (node.xpos, node.ypos)
+			self.n_pos[pos] = node
+			if 'name' in node.get_info():
+				self.n_name[node.get_info(info='name')] = node
 
 	def get_node(self,pid=None, pos=None, name=None):
 		'''
 		gets the reqd node from the graph given, 
 		pid or pos or name of the Node
 		'''
-		pass
+		if pid: 
+			return self.n_pid[pid]  
+        if position:
+        	return self.n_pos[position]
+        if name : 
+        	return self.n_name[name]
+        return None
 
-	def connect(self, src_id, dest_id, rid, wt=0.0, info={},type='oneway'):
+	def connect(self, src_id, dest_id, rid, wt=None, info={},way='twoway'):
 		'''
 		connect the nodes with src, dest id with road rid.
 		'''
-		pass
+		if src_id not in self.n_pid or dest_id not in self.n_pid:
+			pass
+
+		else:
+			s_node = self.n_pid[src_id] 
+			d_node = self.n_pid[dest_id]
+
+			if not wt:
+				wt = (s_node.xpos - d_node.xpos)**2 + (s_node.ypos - d_node.ypos)**2
+
+			if 'angle' not in info:
+				info['angle'] = get_angle(self.get_node(pid=src_id), self.get_node(pid=dest_id))
+
+			rd = Road(rid, d_node, wt, **info)
+			if dest_id not in self.n_pid[src_id].get_children_id():
+				self.n_pid[src_id].add_road(rd)
+
+			if way == 'twoway':
+				info = copy.deepcopy(info)
+				info['angle'] = (angle+180)%360
+				rd = Road(rid, d_node, wt, **info)
+				if src_id not in self.n_pid[dest_id].get_children_id():
+				self.n_pid[dest_id].add_road(rd)
+
 
 	def display(self):
 		'''
 		Display the graph
 		'''
-		pass
+		for node in self.n_pid:
+            #prints all the children nodes for every node in the graph.
+            print 'Tag ID: ',node,'(x= ', self.n_pid[node].xpos,', y= ', self.n_pid[node].ypos, ') -> ',self.n_pid[node].get_children_id()
 
 	def delete_node(self,pid):
 		'''
 		delete the node with specific pid.
 		also automatically delete all roads to and from it.
 		'''
-		pass
+		links = self.n_pid[pid].get_children_id()
 
-	def delete_road(self, rid):
+        for id in links:
+            self.delete_road(pid, id)
+
+        node = self.n_pid.pop(pid)
+        #delete from n_pid dict
+        self.n_name.pop(node.get_info(info='name'), None)
+        #delete from n_name dict
+        return self.n_pos.pop((node.xpos, node.ypos))
+        #delete from n_pos dict, and return the Node object
+
+#This should be delete link, delete road should delete the whole road
+	def delete_road(self, pid1, pid2):
 		'''
 		Remove the road with the given rid
 		'''
+		node = self.n_pid[pid1]
+
+        for road in node.roads:
+            if road.get_dest_pid() == pid2:
+                node.roads.remove(road)
+
+        node = self.n_pid[pid2]
+
+        for road in node.roads:
+            if road.get_dest_pid() == pid1:
+                node.roads.remove(road)
 
 	def update_node_info(self, pid, **info):
 		'''
@@ -73,11 +132,15 @@ class Graph(object):
 		'''
 		#if the other side wants to write a dict, 
 		#it should unpack by **param while passing
-		pass
+		node = self.n_pid[pid]
+        node.update_info(**info)
 
 	def update_road_info(self, rid, **info):
 		'''
 		Update the road with rid = rid with info
 		'''
-		pass
-		
+		if rid in self.road_tags:
+            self.r_tag[rid].update(info)
+        else:
+            self.road_tags[rid] = dict_of_info
+
