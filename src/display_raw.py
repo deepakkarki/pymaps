657f84b85f49499e40b1c9e8e844b1195c8f6608
deepakkarki <deepak6k@gmail.com>
2013-12-21 17:02:27 +0530

    added a raw visualizer for the graph

---
+++ src/display_raw.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import pylab as p
+
+def display(g, show=True):
+	fig = p.figure()
+	
+	for node in g.nodes.values():
+		if len(node.roads) == 0:
+			p.plot(node.xpos,node.ypos, 'bo', color='blue')
+		elif len(node.roads) == 1:
+			p.plot(node.xpos,node.ypos, 'bo', color='black')
+		elif len(node.roads) == 2:
+			p.plot(node.xpos,node.ypos, 'bo', color='green')
+		elif len(node.roads) == 3:
+			p.plot(node.xpos,node.ypos, 'bo', color='red')
+		else:
+			p.plot(node.xpos,node.ypos, 'bo', color='orange')
+		for road in node.roads:
+			p.plot([node.xpos, road.dest.xpos], [node.ypos, road.dest.ypos], color='blue')
+	
+	if show: p.show()
