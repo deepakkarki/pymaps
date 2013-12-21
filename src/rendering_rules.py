6900abae8ae0330b86467de0f84dca43c1adc514
deepakkarki <deepak6k@gmail.com>
2013-12-21 15:37:34 +0530

    defined rendering rules

---
+++ src/rendering_rules.py
@@ -0,0 +1,56 @@
+renderingRules = {
+        'primary': dict(
+                linestyle       = '-',
+                linewidth       = 6,
+                color           ='#ee82ee', 
+                zorder          = -1,
+                ),
+        'primary_link': dict(
+                linestyle       = '-',
+                linewidth       = 6,
+                color           = '#da70d6',
+                zorder          = -1,            
+                ),
+        'secondary': dict(
+                linestyle       = '-',
+                linewidth       = 6,
+                color           = '#d8bfd8',
+                zorder          = -2,            
+                ),
+        'secondary_link': dict(
+                linestyle       = '-',
+                linewidth       = 6,
+                color           = '#d8bfd8',
+                zorder          = -2,            
+                ),
+        'tertiary': dict(
+                linestyle       = '-',
+                linewidth       = 4,
+                color           = (0.0,0.0,0.7),
+                zorder          = -3,            
+                ),
+        'tertiary_link': dict(
+                linestyle       = '-',
+                linewidth       = 4,
+                color           = (0.0,0.0,0.7),
+                zorder          = -3,            
+                ),
+        'residential': dict(
+                linestyle       = '-',
+                linewidth       = 1,
+                color           = (0.1,0.1,0.1),
+                zorder          = -99,            
+                ),            
+        'unclassified': dict(
+                linestyle       = ':',
+                linewidth       = 1,
+                color           = (0.5,0.5,0.5),
+                zorder          = -1,            
+                ),
+        'default': dict(
+                linestyle       = '-',
+                linewidth       = 3,
+                color           = 'b',
+                zorder          = -1,            
+                ),
+    }
